0a0fd41c93638ad6256e24c0274c92a231cb084e
jinjin220 <dreamweaver995@gmail.com>
2019-10-26 12:47:54 +0100

    adding global css

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,8 @@
 import React from "react"
-import Navbar from "./Navbar/Navbar"
-import Footer from "./Footer/Footer"
+import Navbar from "../Navbar/Navbar"
+import Footer from "../Footer/Footer"
+
+import "./layout.css"
 
 const Layout = ({ children }) => {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 import React from "react"
-import Layout from "../components/Layout"
+import Layout from "../components/Layout/Layout"
 
 export default () => <Layout>Hello world!</Layout>
