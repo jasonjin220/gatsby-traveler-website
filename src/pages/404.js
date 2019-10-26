b6454b7e549d90c57a98d5f713ea014ecf4ebcf2
jinjin220 <dreamweaver995@gmail.com>
2019-10-26 17:51:26 +0100

    building the error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,18 @@
 import React from "react"
 import Layout from "../components/Layout/Layout"
+import Banner from "../components/Banner/Banner"
+import { Link } from "gatsby"
 
 const error = () => {
   return (
     <Layout>
-      <h1>OOPS IT'S A DEAD END</h1>
+      <header className="error">
+        <Banner title="some roads have a dead end">
+          <Link to="/" className="btn-white">
+            Back To Home Page
+          </Link>
+        </Banner>
+      </header>
     </Layout>
   )
 }
