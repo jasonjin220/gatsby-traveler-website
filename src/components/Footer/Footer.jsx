fa90134cbb236dc8615d3f91b6d1e2f15018a4ce
jinjin220 <dreamweaver995@gmail.com>
2019-10-26 16:14:04 +0100

    building the footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,43 @@
 import React from "react"
+import { Link } from "gatsby"
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaSquarespace,
+  FaTwitter,
+} from "react-icons/fa"
+
+import "./footer.scss"
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className="footer">
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/tours">Tours</Link>
+        <Link to="/about">About</Link>
+      </div>
+
+      <div className="icons">
+        <Link>
+          <FaFacebookF />
+        </Link>
+        <Link>
+          <FaInstagram />
+        </Link>
+        <Link>
+          <FaTwitter />
+        </Link>
+        <Link>
+          <FaSquarespace />
+        </Link>
+      </div>
+      <div className="copyright">
+        copyright &copy; Traveler travel company {new Date().getFullYear()} all
+        rights reserved
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
