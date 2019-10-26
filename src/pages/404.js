401f3027bb04c359bb225dc845ec7bc4775ecfb4
jinjin220 <dreamweaver995@gmail.com>
2019-10-26 11:44:57 +0100

    creating pages & Layout component

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const error = () => {
+  return (
+    <Layout>
+      <h1>OOPS IT'S A DEAD END</h1>
+    </Layout>
+  )
+}
+
+export default error
---
+++ src/pages/tours.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const tours = () => {
+  return (
+    <Layout>
+      <h1>Tours Page</h1>
+    </Layout>
+  )
+}
+
+export default tours
