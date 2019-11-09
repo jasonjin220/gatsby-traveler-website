91f30ef7a101bdbd22c649158fc06c501a8773c3
jinjin220 <dreamweaver995@gmail.com>
2019-11-09 11:35:48 +0000

    building the TourBanner component

---
+++ src/components/TourBanner/TourBanner.jsx
@@ -0,0 +1,54 @@
+import React from "react"
+import "./tourBanner.scss"
+import Image from "gatsby-image"
+
+const TourBanner = ({ images }) => {
+  const img1 = images[0].fluid
+  const img2 = images[1].fluid
+  const img3 = images[2].fluid
+  const img4 = images[3].fluid
+  const img5 = images[4].fluid
+  return (
+    <main>
+      <div className="tour-banner">
+        <figure className="tour-banner__banner-img--1">
+          <Image
+            fluid={img1}
+            className="tour-banner__banner-img"
+            alt="banner main img"
+          />
+        </figure>
+        <figure className="tour-banner__banner-img--2">
+          <Image
+            fluid={img2}
+            className="tour-banner__banner-img"
+            alt="banner img2"
+          />
+        </figure>
+        <figure className="tour-banner__banner-img--3">
+          <Image
+            fluid={img3}
+            className="tour-banner__banner-img"
+            alt="banner main img3"
+          />
+        </figure>
+        <figure className="tour-banner__banner-img--4">
+          <Image
+            fluid={img4}
+            className="tour-banner__banner-img"
+            alt="banner main img4"
+          />
+        </figure>
+        <figure className="tour-banner__banner-img--5">
+          <Image
+            fluid={img5}
+            className="tour-banner__banner-img"
+            alt="banner main img5"
+          />
+        </figure>
+      </div>
+    </main>
+  )
+}
+
+export default TourBanner
--- src/templates/tour-template.js
+++ src/templates/tour-template.js
@@ -1,7 +1,50 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout/Layout"
+import TourBanner from "../components/TourBanner/TourBanner"
 
-const TourTemplate = () => {
-  return <div>MY TOUR TEMPLATE</div>
+const TourTemplate = ({ data }) => {
+  const {
+    name,
+    price,
+    country,
+    days,
+    description: { description },
+    images,
+    hightlights,
+  } = data.tour
+  return (
+    <Layout>
+      <TourBanner images={images} />
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  query($slug: String!) {
+    tour: contentfulTour(slug: { eq: $slug }) {
+      name
+      price
+      days
+      start(formatString: "dddd MMMM Do, YYYY")
+      country
+      description {
+        description
+      }
+      itinerary {
+        day
+        info
+      }
+      images {
+        fluid {
+          ...GatsbyContentfulFluid
+        }
+      }
+      highlights {
+        info
+      }
+    }
+  }
+`
 
 export default TourTemplate
