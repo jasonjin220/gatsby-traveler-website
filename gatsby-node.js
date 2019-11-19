5df139ff8add0f7ed760a0fc75002bcdd5a45ee3
jinjin220 <dreamweaver995@gmail.com>
2019-11-19 14:14:56 +0000

    programmatically create blog pages from data

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,6 +11,13 @@ exports.createPages = async ({ graphql, actions }) => {
           }
         }
       }
+      posts: allContentfulPost {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
     }
   `)
 
@@ -18,6 +25,16 @@ exports.createPages = async ({ graphql, actions }) => {
     createPage({
       path: `/tours/${node.slug}`,
       component: path.resolve("./src/templates/tour-template.js"),
+      context: {
+        slug: node.slug,
+      },
+    })
+  })
+
+  data.posts.edges.forEach(({ node }) => {
+    createPage({
+      path: `/blog/${node.slug}`,
+      component: path.resolve("./src/templates/blog-template.js"),
       context: {
         slug: node.slug,
       },
---
+++ src/templates/blog-template.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout/Layout"
+import StyledHero from "../components/StyledHero"
+import Banner from "../components/Banner/Banner"
+
+const BlogTemplate = ({ data }) => {
+  console.log(data)
+  const { title, image } = data.blog
+  return (
+    <Layout>
+      <StyledHero opacity="true" img={image.fluid}>
+        <Banner className="banner" title={title}></Banner>
+      </StyledHero>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    blog: contentfulPost(slug: { eq: $slug }) {
+      published(formatString: "D MMMM Y")
+      title
+      intro
+      text {
+        json
+      }
+      slug
+      id: contentful_id
+      image {
+        fluid {
+          ...GatsbyContentfulFluid
+        }
+      }
+    }
+  }
+`
+
+export default BlogTemplate
