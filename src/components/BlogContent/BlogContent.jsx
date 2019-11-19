2ea251223ed7158441d63bab442ad8d130b846ad
jinjin220 <dreamweaver995@gmail.com>
2019-11-19 16:32:37 +0000

    building BlogContent component

---
+++ src/components/BlogContent/BlogContent.jsx
@@ -0,0 +1,64 @@
+import React from "react"
+import RecentPosts from "../RecentPosts/RecentPosts"
+import { useStaticQuery, graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
+import "./blogContent.scss"
+
+const getPosts = graphql`
+  query {
+    posts: allContentfulPost(sort: { fields: published, order: DESC }) {
+      edges {
+        node {
+          published(formatString: "D MMMM Y")
+          title
+          intro
+          slug
+          id: contentful_id
+          image {
+            fluid {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const images = {
+  renderNode: {
+    "embedded-asset-block": node => {
+      console.log(node)
+      return (
+        <img
+          className="blog__image"
+          width="100%"
+          src={node.data.target.fields.file["en-US"].url}
+          alt=""
+        />
+      )
+    },
+  },
+}
+
+const BlogContent = ({ title, published, text: { json } }) => {
+  const response = useStaticQuery(getPosts)
+  const posts = response.posts.edges
+  return (
+    <div className="container">
+      <section className="blog-section">
+        <article className="blog">
+          <h1 className="blog__title">{title}</h1>
+          <p className="blog__published">Posted on {published}</p>
+          <div className="blog__content">
+            {documentToReactComponents(json, images)}
+          </div>
+        </article>
+        <RecentPosts posts={posts} />
+      </section>
+    </div>
+  )
+}
+
+export default BlogContent
--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -3,15 +3,17 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout/Layout"
 import StyledHero from "../components/StyledHero"
 import Banner from "../components/Banner/Banner"
+import BlogContent from "../components/BlogContent/BlogContent"
 
 const BlogTemplate = ({ data }) => {
-  console.log(data)
+  // console.log(data)
   const { title, image } = data.blog
   return (
     <Layout>
       <StyledHero opacity="true" img={image.fluid}>
         <Banner className="banner" title={title}></Banner>
       </StyledHero>
+      <BlogContent {...data.blog} />
     </Layout>
   )
 }
