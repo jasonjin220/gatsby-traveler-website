c4aa38d0dce42aff3b5d2afe40efc94e0c3de8f6
jinjin220 <dreamweaver995@gmail.com>
2019-11-29 16:57:16 +0000

    adding hero image to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,34 @@
 import React from "react"
 import Layout from "../components/Layout/Layout"
+import StyledHero from "../components/StyledHero"
+import Banner from "../components/Banner/Banner"
+import { graphql } from "gatsby"
 
-const about = () => {
+export const query = graphql`
+  query {
+    aboutHeroImage: file(relativePath: { eq: "aboutHero.jpg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4140) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
+
+const about = ({ data }) => {
   return (
     <Layout>
-      <h1>About Page</h1>
+      <StyledHero
+        opacity="true"
+        img={data.aboutHeroImage.childImageSharp.fluid}
+      >
+        <Banner
+          className="banner"
+          title="About Us"
+          info="connect people to life-enriching travel experiences"
+        ></Banner>
+      </StyledHero>
     </Layout>
   )
 }
