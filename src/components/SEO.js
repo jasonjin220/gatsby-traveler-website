108da7ff9ba0d2a2a1c5c4dc5f21ebe4a0817f68
jinjin220 <dreamweaver995@gmail.com>
2020-01-25 10:12:45 +0000

    adding page metadata

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -2,10 +2,35 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
+const getSeoData = graphql`
+  query {
+    site {
+      siteMetadata {
+        siteTitle: title
+        siteDesc: description
+        author
+        siteUrl
+        image
+        twitterUsername
+      }
+    }
+  }
+`
+
 const SEO = ({ title, description }) => {
+  const { site } = useStaticQuery(getSeoData)
+  const {
+    siteDesc,
+    siteTitle,
+    siteUrl,
+    image,
+    twitterUsername,
+  } = site.siteMetadata
+
   return (
-    <Helmet htmlAttributes={{ lang: "en" }} title={title}>
-      <meta name="description" description={description} />
+    <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
+      <meta name="description" content={description} />
+      <meta name="image" content={image} />
     </Helmet>
   )
 }
